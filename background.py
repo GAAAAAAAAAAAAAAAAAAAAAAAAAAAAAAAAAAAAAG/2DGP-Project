38c2b8f53c098480a103d96eac640faceb34a9bd
Desktop <seogaeun@tukorea.ac.kr>
2023-11-12 20:44:14 +0900

    백그라운드 이미지 교체

--- background.py
+++ background.py
@@ -3,7 +3,7 @@ from pico2d import load_image
 
 class Background:
     def __init__(self):
-        self.image = load_image('background.png')
+        self.image = load_image('background1.png')
 
     def draw(self):
         self.image.draw(300, 400)
