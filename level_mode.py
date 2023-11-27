410f4a60e1e9ec18fc7d0415e7572bbddb47fdcb
Desktop <seogaeun@tukorea.ac.kr>
2023-11-28 03:36:55 +0900

    level_mode 구현

---
+++ level_mode.py
@@ -0,0 +1,35 @@
+import game_framework
+from pico2d import *
+import play_mode
+
+def init():
+    global image
+    image = load_image('LevelOfDifficulty.png')
+
+def finish():
+    global image
+    del image
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_e):
+            game_framework.change_mode(play_mode)
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_n):
+            game_framework.change_mode(play_mode)
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_h):
+            game_framework.change_mode(play_mode)
+
+
+def draw():
+    clear_canvas()
+    image.draw(300,400)
+    update_canvas()
+
+
+def update():
+    pass
