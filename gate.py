ef092d81f97dae81600122f229e83a8d6b7fc829
Desktop <seogaeun@tukorea.ac.kr>
2023-12-03 00:33:16 +0900

    level에 따른 속도 변경 및 기문 아래에서 위뢰 올라오기 구현

--- gate.py
+++ gate.py
@@ -1,7 +1,19 @@
 from pico2d import *
 import game_world
+import game_framework
 import random
 import server
+
+PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
+SKIING_SPEED_KMPH = 20.0 # Km / Hour
+SKIING_SPEED_MPM = (SKIING_SPEED_KMPH * 1000.0 / 60.0)
+SKIING_SPEED_MPS = (SKIING_SPEED_MPM / 60.0)
+SKIING_SPEED_PPS = (SKIING_SPEED_MPS * PIXEL_PER_METER)
+
+# Player Action Speed
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 7
 
 class Gate:
     image = None
@@ -9,16 +21,17 @@ class Gate:
         if Gate.image == None:
             Gate.image = load_image('redGate135.png')
 
-        self.x = x if x else random.randint(100, server.background.w - 100)
-        self.y = y if y else random.randint(100, server.background.h - 100)
+        self.x = x if x else random.randint(100, 500)
+        self.y = y if y else random.randint(-10000, 100)
 
         #self.image = load_image('redGate135.png')
 
     def update(self):
+        self.y += SKIING_SPEED_PPS * game_framework.frame_time * server.level
         pass
 
     def draw(self):
-        self.image.draw(self.x - server.background.window_left, self.y - server.background.window_bottom)
+        self.image.draw(self.x, self.y)
         #self.image.clip_draw(0,0,135,45,200,370)
         draw_rectangle(*self.get_bb())  # 튜플을 풀어헤쳐서 인자로 전달.
 
