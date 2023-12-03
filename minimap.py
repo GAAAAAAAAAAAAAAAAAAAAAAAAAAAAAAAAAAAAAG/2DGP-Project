0f9f968c27fc3ddfbec419f8c4d2600535764bc1
GAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAG <seogaeun@tukorea.ac.kr>
2023-12-04 04:35:41 +0900

    미니맵 포인터 이동 구현(거리 완벽한 계산x)

---
+++ minimap.py
@@ -0,0 +1,42 @@
+from pico2d import *
+import game_world
+import game_framework
+import random
+import server
+import play_mode
+
+PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
+SKIING_SPEED_KMPH = 20.0 # Km / Hour
+SKIING_SPEED_MPM = (SKIING_SPEED_KMPH * 1000.0 / 60.0)
+SKIING_SPEED_MPS = (SKIING_SPEED_MPM / 60.0)
+SKIING_SPEED_PPS = (SKIING_SPEED_MPS * PIXEL_PER_METER)
+
+# Player Action Speed
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 7
+
+class Minimap:
+    def __init__(self):
+        self.image = load_image('minimap.png')
+        self.arrow_image = load_image('minimap_arrow.png')
+        self.x = 60
+        self.y = 740
+        self.arrow_y = 790
+
+
+    def update(self):
+        if server.stop == False:
+            self.arrow_y -= SKIING_SPEED_PPS * game_framework.frame_time * server.level * server.boost * 0.01
+        pass
+
+    def draw(self):
+        self.image.clip_draw(0, 0, 50, 100, self.x, self.y)
+        self.arrow_image.clip_draw(0, 0, 100, 100, self.x, self.arrow_y, 10, 10)
+        #draw_rectangle(*self.get_bb())  # 튜플을 풀어헤쳐서 인자로 전달.
+
+    def get_bb(self):
+        return self.x - 300, self.y - 50, self.x + 300, self.y
+
+    def handle_collision(self, group, other):
+        pass
